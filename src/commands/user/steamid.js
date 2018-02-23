f94a82fc19ace8a82a39726840fbeea0ad7110c7
Om Bhende <deletebeach666@gmail.com>
2018-02-23 18:56:22 +0530

    Updated steamid command

--- src/commands/user/steamid.js
+++ src/commands/user/steamid.js
@@ -1,6 +1,9 @@
 const Commando = require("discord.js-commando");
 const request = require("request");
 const Discord = require('discord.js');
+const config = require('../../Config.js');
+var to_json = require('xmljson').to_json;
+
 module.exports = class UserSteamidCommand extends Commando.Command {
     constructor(client) {
         super (client, {
@@ -21,26 +24,20 @@ module.exports = class UserSteamidCommand extends Commando.Command {
         });
     }
     
+
     async run (msg, args, client) {
-        request('https://api.obfam.in/steam/?id=' + args.steamid, function (error, response, body) {
-            if (!error && response.statusCode === 200) {
-                var res = JSON.parse(body);
-                if (res.response.players.length === 0) {
-                    return msg.channel.send("**Uh-oh!** No SteamID found for " + args.steamid)
-                }
-                var steam_user = res.response.players[0];
+        request("http://steamcommunity.com/id/"+args.steamid+"?xml=1", function (error, response, body) {
+            to_json(body, function (error, data) {
                 var user_card = new Discord.RichEmbed()
-                    .setTitle(steam_user.personaname)
-                    .setURL(steam_user.profileurl)
-                    .setColor((steam_user.personastate) ? "#01DF01" : "#FFFFFF")
-                    .addField("SteamID", steam_user.steamid, true)
-                    .addField("Location", steam_user.loccountrycode, true)
-                    .setDescription((steam_user.realname) ? steam_user.realname : '')
-                    .setThumbnail(steam_user.avatarfull)
+                    .setTitle(data.profile.steamID)
+                    .setURL('http://steamcommunity.com/id/' + args.steamid + '/')
+                    .setColor((data.profile.visibilityState) ? "#ffffff" : "#FFFFFF")
+                    .addField("SteamID", data.profile.steamID64, true)
+                    .addField("Location", data.profile.location, true)
+                    .setDescription(data.profile.summary)
+                    .setThumbnail(data.profile.avatarFull)
                 msg.channel.send(user_card);
-            } else {
-                //console.log(response.statusCode);
-            }
+            });
         });
     }
 };

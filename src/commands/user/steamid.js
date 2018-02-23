9cac5779b9025859c6d9b88ac91ce50c87f06599
Om Bhende <deletebeach666@gmail.com>
2018-02-23 20:52:18 +0530

    steamid: Now it shows most played game!

--- src/commands/user/steamid.js
+++ src/commands/user/steamid.js
@@ -29,15 +29,39 @@ module.exports = class UserSteamidCommand extends Commando.Command {
     async run (msg, args, client) {
         request("http://steamcommunity.com/id/"+args.steamid+"?xml=1", function (error, response, body) {
             to_json(body, function (error, data) {
+                if (data.profile === undefined) { return msg.channel.send("**[Error]** Invalid Steam ID"); }
+                if (data.profile.privacyState === "private") {
+                    var user_card2 = new Discord.RichEmbed()
+                    .setTitle(data.profile.steamID)
+                    .setURL('http://steamcommunity.com/id/' + args.steamid + '/')
+                    .setColor((data.profile.visibilityState) ? "#ffffff" : "#FFFFFF")
+                    .addField("SteamID", data.profile.steamID64, true)
+                    .setDescription("This profile is private.")
+                    .setThumbnail(data.profile.avatarFull)
+                    msg.channel.send(user_card2);
+                } else {
+                    var game = "";
+                    var hours = 0;
+                    if (data.profile.mostPlayedGames.mostPlayedGame["0"] == undefined) {
+                        game = data.profile.mostPlayedGames.mostPlayedGame.gameName;
+                        hours = data.profile.mostPlayedGames.mostPlayedGame.hoursOnRecord
+                    } else {
+                        game = data.profile.mostPlayedGames.mostPlayedGame["0"].gameName;
+                        hours = data.profile.mostPlayedGames.mostPlayedGame["0"].hoursOnRecord
+                    }
                 var user_card = new Discord.RichEmbed()
                     .setTitle(data.profile.steamID)
                     .setURL('http://steamcommunity.com/id/' + args.steamid + '/')
                     .setColor((data.profile.visibilityState) ? "#ffffff" : "#FFFFFF")
                     .addField("SteamID", data.profile.steamID64, true)
                     .addField("Location", data.profile.location, true)
+                    .addField("Most Played Game:", game + " -|- Playtime: " + hours + " hours", true)
                     .setDescription(striptags(data.profile.summary))
                     .setThumbnail(data.profile.avatarFull)
-                msg.channel.send(user_card);
+                    msg.channel.send(user_card);
+
+                }
+                
             });
         });
     }
